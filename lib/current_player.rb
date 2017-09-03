5b99f3e3d9b6c0e343cd96e9661db77761b6211e
Chris Roberts <chrisrobertspdx@gmail.com>
2017-09-03 18:43:44 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,15 @@
 def turn_count(board)
-  count = 1
+  count = 0
   board.each do |space|
     if space == 'X' || space == 'O'
       count += 1
     end
   end
+  count
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 1
+  if turn_count(board) % 2 == 0
     "X"
   else
     "O"
